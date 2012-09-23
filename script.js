4275198339f85ae20f5ead1cd91f98fb0f295107
Swizec Teller <swizec@swizec.com>
2012-09-23 14:21:55 +0200

    imported express and co.

--- script.js
+++
@@ -1,8 +0,0 @@
-
-var swizec = new Gh3.User("swizec");
-var repos = new Gh3.Repositories(swizec);
-
-repos.fetch({page: 1, per_page: 500, direction: "desc"},
-            function () {
-                console.log("hello");
-            });
