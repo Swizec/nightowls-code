3bf61f17293d3a96eaf18867064ffc711dd8a91f
Swizec Teller <swizec@swizec.com>
2012-09-26 01:49:47 +0200

    displaying a rough punchcard

--- public/javascripts/punchcard-graph.js
+++ public/javascripts/punchcard-graph.js
@@ -1,45 +1,21 @@
 
-var minvalx = 0, maxvalx = 23,
-    minvaly = 0, maxvaly = 6,
-    sampsize = punchcard_data.length;
+var bucket = {w: 30, h: 30},
+    max = _.max(_.map(punchcard_data,
+                      function (d) { return d[2]; }));
 
-var w = 800,
-    h = 500,
-    p = 80,
-    x = d3.scale.linear().domain([0, maxvalx]).range([0, w]),
-    y = d3.scale.linear().domain([0, maxvaly]).range([h, 0]);
+console.log(max);
 
-var vis = d3
-        .select("#punchcard")
-        .data(punchcard_data)
-        .enter().append("svg:svg")
-        .attr("width", w+p*2)
-        .attr("height", h+p*2)
-        .append("svg:g")
-        .attr("transform", "translate("+p+","+p+")");
+var svg = d3.select("#punchcard")
+        .append("svg")
+        .attr("width", bucket.w*(24+2))
+        .attr("height", bucket.h*(7+2));
 
-var rules = vis
-        .selectAll("g.rule")
-        .data(x.ticks(1))
-        .enter().append("svg:g")
-        .attr("class", "rule");
-
-rules.append("svg:line")
-    .attr("x1", x).attr("x2", x)
-    .attr("y1", 0).attr("y2", h-1);
-
-rules.append("svg:line")
-    .attr("class", function (d) { return d ? null : "axis"; })
-    .data(y.ticks(1))
-    .attr("y1", y).attr("y2", y)
-    .attr("x1", 0)
-    .attr("x2", w-10);
-
-vis.selectAll("circle.line")
+svg.selectAll("circle")
     .data(punchcard_data)
-    .enter().append("svg:circle")
-    .attr("class", "line")
-    .attr("fill", "#000")
-    .attr("cx", function (d) { return d[1]; })
-    .attr("cy", function (d) { return d[0]; })
-    .attr("r", function (d) { return Math.sqrt(d[2] / Math.PI); });
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return d[1]*bucket.w+bucket.w; })
+    .attr("cy", function (d) { return d[0]*bucket.h+bucket.h; })
+    .attr("r", function (d) { return Math.floor(d[2]/max*(bucket.w/3)); });
+
+
