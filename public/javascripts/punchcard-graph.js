872ab8505f9fb3187cfc726a06aec4216d304788
Swizec Teller <swizec@swizec.com>
2012-09-26 01:24:48 +0200

    turns out d3 is quite difficult to use

--- public/javascripts/punchcard-graph.js
+++ public/javascripts/punchcard-graph.js
@@ -1,6 +1,45 @@
 
-(function ($) {
+var minvalx = 0, maxvalx = 23,
+    minvaly = 0, maxvaly = 6,
+    sampsize = punchcard_data.length;
 
-    console.log("HELLO");
+var w = 800,
+    h = 500,
+    p = 80,
+    x = d3.scale.linear().domain([0, maxvalx]).range([0, w]),
+    y = d3.scale.linear().domain([0, maxvaly]).range([h, 0]);
 
-})(jQuery);
+var vis = d3
+        .select("#punchcard")
+        .data(punchcard_data)
+        .enter().append("svg:svg")
+        .attr("width", w+p*2)
+        .attr("height", h+p*2)
+        .append("svg:g")
+        .attr("transform", "translate("+p+","+p+")");
+
+var rules = vis
+        .selectAll("g.rule")
+        .data(x.ticks(1))
+        .enter().append("svg:g")
+        .attr("class", "rule");
+
+rules.append("svg:line")
+    .attr("x1", x).attr("x2", x)
+    .attr("y1", 0).attr("y2", h-1);
+
+rules.append("svg:line")
+    .attr("class", function (d) { return d ? null : "axis"; })
+    .data(y.ticks(1))
+    .attr("y1", y).attr("y2", y)
+    .attr("x1", 0)
+    .attr("x2", w-10);
+
+vis.selectAll("circle.line")
+    .data(punchcard_data)
+    .enter().append("svg:circle")
+    .attr("class", "line")
+    .attr("fill", "#000")
+    .attr("cx", function (d) { return d[1]; })
+    .attr("cy", function (d) { return d[0]; })
+    .attr("r", function (d) { return Math.sqrt(d[2] / Math.PI); });
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@
 var fetching = require('../lib/fetching');
 
 exports.index = function(req, res){
-    console.log("FETCHING");
     fetching.full_punchcard('', function (err, punchcard) {
         res.render('index', { title: 'Express',
                               punchcard: JSON.stringify(punchcard)});
