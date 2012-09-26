3339a49b81129eb111e0f12bfbd1028816f47d74
Swizec Teller <swizec@swizec.com>
2012-09-26 18:49:36 +0200

    getting token from github

--- def_secrets.js
+++ def_secrets.js
@@ -1,3 +1,6 @@
 
 exports.test_username = '';
 exports.test_password = '';
+
+exports.client_id = '';
+exports.client_secret = '';
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,25 @@
  * GET users listing.
  */
 
+var github = require('octonode'),
+    secrets = require('../secrets');
+
 exports.list = function(req, res){
   res.send("respond with a resource");
+};
+
+var auth_url = github.auth.config({
+    id: secrets.client_id,
+    secret: secrets.client_secret
+}).login(['user', 'repo']);
+
+exports.login = function(req, res) {
+    res.redirect(301, auth_url);
+};
+
+exports.auth = function (req, res) {
+    github.auth.login(req.param('code'), function (err, token) {
+        res.send(token);
+    });
+
 };
