a96788a3b6f650748203fad350f0532be4e5c915
Swizec Teller <swizec@swizec.com>
2012-09-27 02:33:52 +0200

    smooth transition into data

--- public/js/punchcard-graph.js
+++ public/js/punchcard-graph.js
@@ -1,18 +1,16 @@
 
-d3.json(Data_url, function (punchcard_data) {
-
 var w = 940,
     h = 300,
     pad = 20,
-    x_pad = 100,
-    max_r = _.max(_.map(punchcard_data,
-                      function (d) { return d[2]; }));
+    x_pad = 100;
+
+var svg = d3.select("#punchcard")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h);
 
 var x = d3.scale.linear().domain([0, 23]).range([x_pad, w-pad]),
-    y = d3.scale.linear().domain([0, 6]).range([pad, h-pad*2]),
-    r = d3.scale.linear()
-        .domain([0, d3.max(punchcard_data, function (d) { return d[2]; })])
-        .range([0, 12]);
+    y = d3.scale.linear().domain([0, 6]).range([pad, h-pad*2]);
 
 var xAxis = d3.svg.axis().scale(x).orient("bottom")
         .ticks(24)
@@ -26,11 +24,6 @@ var xAxis = d3.svg.axis().scale(x).orient("bottom")
             return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][d];
         });
 
-var svg = d3.select("#punchcard")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
-
 svg.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(0, "+(h-pad)+")")
@@ -41,14 +34,23 @@ svg.append("g")
     .attr("transform", "translate("+(x_pad-pad)+", 0)")
     .call(yAxis);
 
-svg.selectAll("circle")
-    .data(punchcard_data)
-    .enter()
-    .append("circle")
-    .attr("class", "circle")
-    .attr("cx", function (d) { return x(d[1]); })
-    .attr("cy", function (d) { return y(d[0]); })
-    .attr("r", function (d) { return r(d[2]); });
 
+d3.json(Data_url, function (punchcard_data) {
+    var max_r = _.max(_.map(punchcard_data,
+                      function (d) { return d[2]; })),
+        r = d3.scale.linear()
+            .domain([0, d3.max(punchcard_data, function (d) { return d[2]; })])
+            .range([0, 12]);
 
+    svg.selectAll("circle")
+        .data(punchcard_data)
+        .enter()
+        .append("circle")
+        .attr("class", "circle")
+        .attr("cx", function (d) { return x(d[1]); })
+        .attr("cy", function (d) { return y(d[0]); })
+        .attr("r", function (d) { return 0; })
+        .transition()
+        .duration(800)
+        .attr("r", function (d) { return r(d[2]); });
 });
